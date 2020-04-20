931f5136755d6566997f2eea99a37b62104c9161
Vikas Bhardwaj <bhardwajvicky@hotmail.com>
2020-04-20 20:48:09 +1000

    post gitignore changes

--- bin/Release/netcoreapp3.1/publish/wwwroot/PSScripts/test.ps1
+++
@@ -1,4 +0,0 @@
-Get-Process -Name WmiPrvSE | Select-Object -Property BasePriority,Id,SessionId,WorkingSet |
-  Export-Csv -Path .\WmiData.csv -NoTypeInformation
-$var1 = gpresult /r
-$var1 | out-file -filepath ".\wwwroot\ScriptOutput\Testoutput.csv"
